f9f8b1767e970076504f10c70b7f73d3539acc70
Andrew Klenner <atklenner@gmail.com>
2021-10-04 20:59:46 -0700

    Fix operator chaining

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 let display = '0';
+let operator;
+let numberStore;
+let evaluated = false;
 
 function add(a, b) {
   return a + b;
@@ -16,13 +19,21 @@ function divide(a, b) {
   return a / b;
 }
 
-function operate(operator, a, b) {
-  return operator(a, b);
-}
-
 //this section handles all the digits and display updates
 const digits = document.querySelectorAll('.digit');
 const digitDisplay = document.querySelector('.display');
+
+function updateDisplay(str) {
+  if(str.length > 9) {
+    str = parseFloat(str);
+    console.log(str);
+    str *= Math.pow(10, 8);
+    str = Math.round(str);
+    str /= Math.pow(10, 8);
+    console.log(str);
+  }
+  digitDisplay.textContent = str.toString();
+}
 
 digits.forEach((digit) => {
   digit.addEventListener('click', (element) => {
@@ -31,7 +42,7 @@ digits.forEach((digit) => {
     } else if(displayLength() < 9) {
       display += element.target.id;
     }
-    digitDisplay.textContent = display;
+    updateDisplay(display);
   });
 });
 
@@ -46,7 +57,7 @@ const zeroButton = document.querySelector('.zero');
 zeroButton.addEventListener('click', () => {
   if(display !== '0' && displayLength() < 9) {
     display += '0';
-    digitDisplay.textContent = display;
+    updateDisplay(display);
   }
 });
 
@@ -55,7 +66,7 @@ const decimalButton = document.querySelector('.decimal');
 decimalButton.addEventListener('click', () => {
   if(!display.includes('.')) {
     display += '.';
-    digitDisplay.textContent = display;
+    updateDisplay(display);  
   }
 });
 
@@ -65,10 +76,10 @@ const backButton = document.querySelector('.backspace');
 backButton.addEventListener('click', () => {
   if(display.length > 1) {
     display = display.substring(0, display.length - 1);
-    digitDisplay.textContent = display;
+    updateDisplay(display);  
   } else {
     display = '0';
-    digitDisplay.textContent = display;
+    updateDisplay(display);  
   }
 });
 
@@ -76,5 +87,48 @@ const clearButton = document.querySelector('.clear');
 
 clearButton.addEventListener('click', () => {
   display = '0';
-  digitDisplay.textContent = display;
+  updateDisplay(display);
+  operator = undefined;
+  numberStore = undefined;
+  evaluated = false;
 });
+
+//this section handles the operations and actual calculating
+function operate() {
+  if(numberStore && operator) {
+    display = operator(numberStore, +display).toString();
+    updateDisplay(display);
+    numberStore = undefined;
+    evaluated = true;
+  }
+}
+
+function operatorUpdate(op) {
+  operate();
+  operator = op;
+  numberStore = +display;
+  display = '0';
+  if(!evaluated) {
+    updateDisplay(display);
+  }
+}
+
+const equalsButton = document.querySelector('.equals');
+
+equalsButton.addEventListener('click', () => operate());
+
+const addButton = document.querySelector('.add');
+
+addButton.addEventListener('click', () => operatorUpdate(add));
+
+const subtractButton = document.querySelector('.subtract');
+
+subtractButton.addEventListener('click', () => operatorUpdate(subtract));
+
+const multiplyButton = document.querySelector('.multiply');
+
+multiplyButton.addEventListener('click', () => operatorUpdate(multiply));
+
+const divideButton = document.querySelector('.divide');
+
+divideButton.addEventListener('click', () => operatorUpdate(divide));
